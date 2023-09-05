859caf084be621f453cc2abc99e5323de9234134
Pedro.Isidorio <pedro.henrique@defensoria.mg.def.br>
2023-09-05 12:28:59 -0300

    feat: criação da classe para validação

---
+++ src/script/data/validacao.js
@@ -0,0 +1,77 @@
+const rex = /^[A-Z a-z]+$/
+
+export class Validation {
+    valid;
+    validations;
+
+    constructor() {
+        this.validations = [
+            "data-required",
+            "data-notNumber",
+            "data-isNumber",
+            "data-minValue",
+            "data-maxValue",
+        ];
+        this.valid = true;
+    }
+
+    validate(form) {
+        const validacoes_atuais = document.querySelectorAll('form .error-validation');
+        if (validacoes_atuais.length > 0) this.limpar_validations(validacoes_atuais)
+
+        const inputsArray = [...form.getElementsByTagName('input')];
+
+        inputsArray.forEach((input) => {
+            this.validations.forEach((val) => {
+                const attributeVal = input.getAttribute(val)
+
+                if (attributeVal != null) {
+                    const method = val.replace('data-', '')
+                    this[method](input, attributeVal)
+                }
+            })
+        })
+    }
+
+
+    required(input) {
+        if (input.value !== '') return
+        this.setError(input, `Este campo é obrigatório`)
+    }
+
+    notNumber(input) {
+        if (rex.test(input.value)) return
+        this.setError(input, `Este campo não aceita números nem caracteres especiais`)
+    }
+
+    isNumber(input) {
+        if (!rex.test(input.value)) return
+        this.setError(input, `Este campo aceita somente números`)
+    }
+
+    minValue(input, qnt) {
+        if (input.value >= qnt) return
+        this.setError(input, `A idade deve ser no minimo de ${qnt}`)
+    }
+
+    maxValue(input, qnt) {
+        if (input.value <= qnt) return
+        this.setError(input, `A idade deve ser no maximo de ${qnt}, tá na hora de aposentar!`)
+    }
+
+    setError(input, msg) {
+        const errosQty = input.parentNode.querySelector('.error-validation');
+        if (errosQty === null) {
+            const tamplate = document.querySelector('.error-validation').cloneNode(true);
+            tamplate.textContent = msg;
+            const inputParent = input.parentNode;
+            tamplate.classList.remove('tamplate');
+            inputParent.appendChild(tamplate);
+            this.valid = false
+        }
+    }
+
+    limpar_validations(validation) {
+        validation.forEach(el => el.remove());
+    }
+}
--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,14 @@
-import { Gerente } from "./modules/gerente.js";
-import { Desenvolvedor } from "./modules/desenvolvedor.js";
+// import { Gerente } from "./modules/gerente.js";
+// import { Desenvolvedor } from "./modules/desenvolvedor.js";
+import { Validation } from "./data/validacao.js";
 
+const form = document.getElementById("form");
+const submit = document.getElementById("submit");
+
+submit.addEventListener('click', function (e) {
+    const validator = new Validation();
+    e.preventDefault();
+    validator.validate(form);
+    
+    console.log(validator.valid)
+});
