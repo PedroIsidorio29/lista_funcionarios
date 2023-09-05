c2094484d8f9d2875a4df9f533d0c35db0d3185c
Pedro.Isidorio <pedro.henrique@defensoria.mg.def.br>
2023-09-05 15:58:15 -0300

    chore: estilização do form

--- src/script/main.js
+++ src/script/main.js
@@ -4,11 +4,25 @@ import { Validation } from "./data/validacao.js";
 
 const form = document.getElementById("form");
 const submit = document.getElementById("submit");
+const toggle = document.getElementById("foo");
 
 submit.addEventListener('click', function (e) {
     const validator = new Validation();
     e.preventDefault();
     validator.validate(form);
-    
+
     console.log(validator.valid)
+    setFuncionarioList()
+});
+
+function setFuncionarioList(){
+
+    const inputsArray = [...form.getElementsByTagName('input')];
+    console.log(inputsArray)
+    inputsArray.forEach((i)=>console.log(i.name))
+// const asd = new Gerente({})
+}
+
+toggle.addEventListener('click', function (e) {
+console.log('aqui')
 });
