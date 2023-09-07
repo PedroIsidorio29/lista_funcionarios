3b314b8b2047e7578172ab9a8de36a6a3e58b9ed
Pedro Souza <pedroisidorio1@gmail.com>
2023-09-07 14:08:59 -0300

    wip: inserção da classe na construção do objeto

--- src/script/main.js
+++ src/script/main.js
@@ -5,38 +5,51 @@ import { Validation } from "./data/validacao.js";
 const form = document.getElementById("form");
 const submit = document.getElementById("submit");
 const toggle = document.querySelector('#foo');
-var validator;
-
 
 submit.addEventListener('click', (e) => {
-    validator = new Validation(toggle.checked)
+    const validator = new Validation(toggle.checked)
     e.preventDefault();
     validator.validate(form);
-    if (validator.valid) setFuncionarioList();
+    if (validator.valid) setFuncionarioList(validator);
 });
 
-function setFuncionarioList() {
+function setFuncionarioList(valid) {
     const inputsArray = [...form.getElementsByTagName('input')];
-    console.log(inputsArray)
-    inputsArray.forEach((i) => console.log(i.name))
-    const asd = new Gerente()
+
+    const funcionario = { nome, idade, cargo }
+
+    inputsArray.forEach((i) => {
+        console.log(i.name)
+        console.log(i.value)
+    })
+    // if (valid.isGerente)
+    //     console.log('asda')
+    // const asd = new Gerente()
 }
 
 toggle.addEventListener('click', () => {
     const label = document.querySelector("#funcionario_label")
     const gerenteInput = document.querySelector("#gerente-input")
     const desenvolvedorInput = document.querySelector("#desenvolvedor-input")
-    
+    const departamento = document.getElementById("departamento")
+    const linguagem = document.getElementById("linguagem")
+
     if (toggle.checked) {
         label.textContent = 'Gerente';
         gerenteInput.style.display = 'block'
         desenvolvedorInput.style.display = 'none'
+        departamento.value = ''
+        const dev = desenvolvedorInput.getElementsByTagName('p')
+        if (dev.length)
+            dev[0].remove()
     }
-    else { 
+    else {
         label.textContent = 'Desenvolvedor';
         gerenteInput.style.display = 'none'
         desenvolvedorInput.style.display = 'block'
-     }
-
-
+        linguagem.value = ''
+        const gerente = gerenteInput.getElementsByTagName('p')
+        if (gerente.length)
+            gerente[0].remove()
+    }
 });
